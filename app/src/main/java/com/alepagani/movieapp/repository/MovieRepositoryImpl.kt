9c82965864320c82e8d8d0dba1777babde547048
AlejandroPaganiTricks <alejandropagani@7r1ck.com>
2024-04-16 09:20:19 -0300

    Chequeo de internet

--- app/src/main/java/com/alepagani/movieapp/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/alepagani/movieapp/repository/MovieRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.alepagani.movieapp.repository
 
 import android.util.Log
+import com.alepagani.movieapp.core.InternetCheck
 import com.alepagani.movieapp.data.local.LocalMovieDataSource
 import com.alepagani.movieapp.data.model.Movie
 import com.alepagani.movieapp.data.model.MovieResponse
@@ -12,26 +13,38 @@ class MovieRepositoryImpl(
     private val localDataSource: LocalMovieDataSource
 ) : MovieRepository {
     override suspend fun getUpcomingMovies(): List<Movie> {
-        dataSource.getUpcomingMovies().results.forEach {
-            localDataSource.saveMovie(it.toMovieEntity("upcoming"))
+        return if (InternetCheck.isNetworkAvailable()) {
+            dataSource.getUpcomingMovies().results.forEach {
+                localDataSource.saveMovie(it.toMovieEntity("upcoming"))
+            }
+            //Log.d("ALEJANDRO", localDataSource.getUpcomingMovies().toString() )
+            localDataSource.getUpcomingMovies()
+        } else {
+            localDataSource.getUpcomingMovies()
         }
-        //Log.d("ALEJANDRO", localDataSource.getUpcomingMovies().toString() )
-        return localDataSource.getUpcomingMovies()
     }
 
     override suspend fun getTopRateMovies(): List<Movie> {
-        dataSource.getUpcomingMovies().results.forEach {
-            localDataSource.saveMovie(it.toMovieEntity("top_rated"))
+        return if (InternetCheck.isNetworkAvailable()) {
+            dataSource.getUpcomingMovies().results.forEach {
+                localDataSource.saveMovie(it.toMovieEntity("top_rated"))
+            }
+            //Log.d("ALEJANDRO", "TOPrated ${localDataSource.getTopRateMovies().toString()}" )
+            localDataSource.getTopRateMovies()
+        } else {
+            localDataSource.getTopRateMovies()
         }
-        //Log.d("ALEJANDRO", "TOPrated ${localDataSource.getTopRateMovies().toString()}" )
-        return localDataSource.getTopRateMovies()
     }
 
     override suspend fun getPopularMovies(): List<Movie> {
-        dataSource.getUpcomingMovies().results.forEach {
-            localDataSource.saveMovie(it.toMovieEntity("popular"))
+        return if (InternetCheck.isNetworkAvailable()) {
+            dataSource.getUpcomingMovies().results.forEach {
+                localDataSource.saveMovie(it.toMovieEntity("popular"))
+            }
+            //Log.d("ALEJANDRO","POPULARES ${localDataSource.getPopularMovies().toString()}" )
+            localDataSource.getPopularMovies()
+        } else {
+            localDataSource.getPopularMovies()
         }
-        //Log.d("ALEJANDRO","POPULARES ${localDataSource.getPopularMovies().toString()}" )
-        return localDataSource.getPopularMovies()
     }
 }
